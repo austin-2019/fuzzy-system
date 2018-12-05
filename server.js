b0b2a5ebe8d66f50547c98b56e72eb890b9e82a8
austin-2019 <sharon.austin.2019@gmail.com>
2018-12-04 19:58:42 -0500

    Added LaunchNode boilerplate

---
+++ server.js
@@ -0,0 +1,40 @@
+"use strict";
+
+let express = require("express");
+let logger = require("morgan");
+let path = require("path");
+
+let config = require("./config");
+
+let requestDebug = require("request-debug");
+let requestJs = require("request");
+
+if (config.get("env") !== "production") {
+	requestDebug(requestJs);
+}
+
+let app = express();
+
+app.set("views", path.join(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+if (config.get("env") !== "production") {
+	app.use(logger("dev"));
+}
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// <Routes>
+
+app.get("/", function(request, response) {
+	config.get("apiKey");
+
+	response.render("pages/index");
+});
+
+// </Routes>
+
+app.listen(config.get("port"), function() {
+	console.log("Listening on port " + this.address().port);
+});
